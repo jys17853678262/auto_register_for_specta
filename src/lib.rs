83c07b99f3a9b60d4b2e976674214d6b8d2dbc33
Super-J <jys17853678262@163.com>
2026-03-22 19:13:48 +0800

    修改接口文件

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 mod core;
 #[allow(unused_imports)]
-pub use local_macro::{auto_add_type_impl, auto_collect_command, auto_collect_struct_type};
+pub use local_macro::{auto_add_type_impl, auto_collect_command_spec, auto_collect_struct_spec};
